f6f5d0deb21111e5b9cf6cc6e52c1a03c5c65907
poyingHAHA <poying.code@gmail.com>
2022-01-26 10:00:56 +0800

    Add 8-Promise and clearify 4-callback

--- 4-callback.js
+++ 4-callback.js
@@ -1,51 +1,13 @@
-setTimeout(() => {
-    console.log('Two seconds are up')
-}, 2000)
-
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name)=>{
-    return name.length <= 4
-})
-
-// gecode finishes almost immediately. And if you don't return something from a function, we know that JS will implivitly return undefined.
-// const gecode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
-
-//         return data
-//     }, 2000)
-// }
-
-// const data = gecode('lukang')
-// console.log(data)
-
-const gecode = (address, callback) => {
-    setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
-
-        callback(data)
+const doWorkCallback = (callback) => {
+    setTimeout(()=>{
+        callback(undefined, [1, 4, 7])
     }, 2000)
 }
 
-gecode('Taiwan', (data)=>{
-    console.log(data)
-})
+doWorkCallback((error, result) => {
+    if(error){
+        return console.log(error)
+    }
 
-// Challenge
-
-const add = (x, y, callback) => {
-    setTimeout(() => {
-        const sum = x+y
-        callback(sum) 
-    }, 2000)
-}
-
-add(1, 4, (sum) => {
-    console.log(sum) //Should print: 5
+    console.log(result)
 })
---
+++ 8-promises.js
@@ -0,0 +1,24 @@
+const doWorkPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+         // In the past with the callback pattern, we would signify this by calling callback in one of two ways.
+         // In Promise, what we have are two separate functions I have resolve and reject.
+        reject('This is my error!') // If rejet is called then the later functions won't be called
+        resolve([2, 3, 2])
+    }, 2000)
+})
+
+// .then allows us to register a function to run when things go well
+// When we're working with promises, we have two separate functions, only one of which will ever run.
+// Either resolve or reject is called the promise is done and it's value or state can't change.
+doWorkPromise.then((result) => {
+    console.log('Success!', result)
+}).catch((error) => {
+    console.log('Error!', error)
+})
+
+//
+//                                     fulfilled
+//                                   /
+// Promise     --  pending      -->
+//                                   \
+//                                     rejected
